8869c7a055480bcc11b4ab2f0b5adfd50d1f304a
Ciscoo91 <franciscomambo91@gmail.com>
2018-11-22 18:45:34 +0100

    Edited tosekwatv view and added youtube videos to the page

--- server.js
+++ server.js
@@ -3,12 +3,22 @@ const app = express();
 const ejs = require('ejs');
 const port = process.env.PORT || 4200;
 const path = require("path");
+const fetch = require('node-fetch');
+
+// Imports
+
+const config = require('./config/config.js');
+
+// Youtube API url
+const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLgZ-2UL8fjcQ5SYWOko2E2cD0HtwZ3FoI&key=${config.key}`;
 
 //Middlewares
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "config")));
+
 //Routes
 
 app.get('/', (req, res) => {
@@ -28,7 +38,13 @@ app.get('/contact', (req, res) => {
 });
 
 app.get('/tosekwatv', (req, res) => {
-    res.render("tosekwatv");
+    console.log(config);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            res.render("tosekwatv", { fetchData: data });
+        })
+        .catch(err => { console.log(err) });
 });
 
 app.listen(port);

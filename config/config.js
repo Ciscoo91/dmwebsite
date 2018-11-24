6fafd32d347dd7e08ff34a7942d4f6834f038442
Ciscoo91 <franciscomambo91@gmail.com>
2018-11-24 14:07:51 +0100

    Add Config files and model

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const API_KEY = "AIzaSyCeL6YdmiqemrOpSChpsDOCdmgZB8n0KVc";
+const API_TOKEN = "4048894955-hjebl197boggmpbm6sfkd3bfv22oskn3.apps.googleusercontent.com";
+const YT_ID = "UCamUMOQOyZbfxzvf3ujm2pg";
+
+const youtubeData = {
+  key: API_KEY,
+  id: API_TOKEN,
+  channel: YT_ID
+}
+
+let configData = () => {
+  return youtubeData;
+}
+
+module.exports = youtubeData;

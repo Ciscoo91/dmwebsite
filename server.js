f7f905c64864175c479ce8b2fa89d4b871ade16a
Ciscoo91 <franciscomambo91@gmail.com>
2018-11-24 14:01:24 +0100

    Add Database to the Contact Form

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const ejs = require('ejs');
 const port = process.env.PORT || 4200;
 const path = require("path");
 const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+let urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
 // Imports
 
@@ -12,12 +14,17 @@ const config = require('./config/config.js');
 // Youtube API url
 const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PLgZ-2UL8fjcQ5SYWOko2E2cD0HtwZ3FoI&key=${config.key}`;
 
+// Mongoose
+
+let Message = require('./model/database.js');
+
+
 //Middlewares
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "config")));
+
 
 //Routes
 
@@ -35,6 +42,40 @@ app.get('/biographie', (req, res) => {
 
 app.get('/contact', (req, res) => {
     res.render("contact");
+});
+
+app.post('/contact', urlEncodedParser, (req, res) => {
+    if (!req.body) {
+        console.error(req);
+        res.sendStatus(500);
+    } else {
+        let formData = req.body;
+        console.log("form data: ", formData);
+
+        // Get request params
+        let firstName = req.body.firstname;
+        let lastName = req.body.lastname;
+        let email = req.body.email;
+        let message = req.body.comment;
+
+        // Create a new message Model
+        let postedMessage = new Message({
+            firstname: firstName,
+            lastname: lastName,
+            email: email,
+            message: message
+        });
+
+        // Save this Object in the database
+        postedMessage.save((err, postedMessage) => {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('The message was posted successfuly');
+        });
+
+        res.redirect('contact');
+    }
 });
 
 app.get('/tosekwatv', (req, res) => {
